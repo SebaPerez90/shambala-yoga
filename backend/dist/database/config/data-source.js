4170a9b273d3be44f5c711ff6b7419c82a092a0a
SebaPerez90 <sebastianalbertoperez90@gmail.com>
2024-04-20 22:11:01 -0300

    .

--- backend/dist/database/config/data-source.js
+++ backend/dist/database/config/data-source.js
@@ -3,17 +3,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppDataSource = void 0;
 const typeorm_1 = require("typeorm");
 const dotenv_1 = require("dotenv");
+const user_entity_1 = require("../entities/user.entity");
+const credential_entity_1 = require("../entities/credential.entity");
+const appointment_entity_1 = require("../entities/appointment.entity");
 (0, dotenv_1.config)();
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "postgres",
     host: "localhost",
     port: 5432,
     username: "postgres",
-    password: process.env.POSTGRESQL_PASSWORD,
+    password: "Greedisgoodd1234_",
     database: "henry",
     synchronize: true,
-    logging: true,
-    entities: [],
+    // dropSchema: true,
+    logging: ["error"],
+    entities: [user_entity_1.User, credential_entity_1.Credential, appointment_entity_1.Appointment],
     subscribers: [],
     migrations: [],
 });
--- backend/dist/index.js
+++ backend/dist/index.js
@@ -8,9 +8,13 @@ const data_source_1 = require("./database/config/data-source");
 require("reflect-metadata");
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
-data_source_1.AppDataSource.initialize().then((res) => {
+data_source_1.AppDataSource.initialize()
+    .then((res) => {
     console.log("data base conection was successfully");
     server_1.default.listen(process.env.PORT, () => {
         console.log(`server running on port ${process.env.PORT}`);
     });
+})
+    .catch((err) => {
+    console.error("Error during Data Source initialization", err);
 });
